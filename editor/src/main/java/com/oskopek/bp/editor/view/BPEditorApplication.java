07239ceb35c4c7082336f4d491435dcc88e625bb
Ondrej Skopek <skopekondrej@gmail.com>
2016-03-25 18:20:49 +0100

    Added stubs

---
+++ editor/src/main/java/com/oskopek/bp/editor/view/BPEditorApplication.java
@@ -0,0 +1,50 @@
+package com.oskopek.bp.editor.view;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+/**
+ * StudyGuide JavaFX main class.
+ */
+public class BPEditorApplication extends Application {
+
+    private Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("StudyGuide");
+        this.primaryStage.getIcons().add(new Image(BPEditorApplication.class.getResourceAsStream("logo_64x64.png")));
+
+        initRootLayout();
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    private void initRootLayout() {
+        Scene scene = new Scene(null);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * Get the main {@link javafx.stage.Window} element of the app.
+     *
+     * @return non-null
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ editor/src/main/java/com/oskopek/bp/editor/view/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * A basic JavaFX view of the model.
+ */
+package com.oskopek.bp.editor.view;
