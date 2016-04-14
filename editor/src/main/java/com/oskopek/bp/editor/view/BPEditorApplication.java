dc91f4742a23c88e4f406a5f31a72105588b98af
Ondrej Skopek <skopekondrej@gmail.com>
2016-04-14 13:35:10 +0200

    Fixed logo path

--- editor/src/main/java/com/oskopek/bp/editor/view/BPEditorApplication.java
+++ editor/src/main/java/com/oskopek/bp/editor/view/BPEditorApplication.java
@@ -3,6 +3,7 @@ package com.oskopek.bp.editor.view;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -24,7 +25,9 @@ public class BPEditorApplication extends Application {
      * Initializes the root layout.
      */
     private void initRootLayout() {
-        Scene scene = new Scene(null);
+        VBox rootLayout = new VBox(); // TODO continue here with the layouts
+
+        Scene scene = new Scene(rootLayout);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
