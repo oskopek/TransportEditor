72d84e46133c28675bce305c270f8b5c6a039a46
Ondrej Skopek <skopekondrej@gmail.com>
2016-04-14 15:58:44 +0200

    Added a localization resource bundle

--- editor/src/main/java/com/oskopek/bp/editor/weld/FXMLLoaderProducer.java
+++ editor/src/main/java/com/oskopek/bp/editor/weld/FXMLLoaderProducer.java
@@ -1,10 +1,11 @@
-package com.oskopek.bp.editor.view;
+package com.oskopek.bp.editor.weld;
 
 import javafx.fxml.FXMLLoader;
 
 import javax.enterprise.inject.Instance;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
+import java.util.ResourceBundle;
 
 /**
  * Created by skopeko on 14.4.16.
@@ -16,7 +17,9 @@ public class FXMLLoaderProducer {
 
     @Produces
     public FXMLLoader createLoader() {
+        ResourceBundle bundle = ResourceBundle.getBundle("com.oskopek.bp.editor.view.messages");
         FXMLLoader loader = new FXMLLoader();
+        loader.setResources(bundle);
         loader.setControllerFactory(param -> instance.select(param).get());
         return loader;
     }
