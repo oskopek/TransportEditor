6da2060b5e32056822c5dd2c47964d1aff543585
Ondrej Skopek <skopekondrej@gmail.com>
2016-05-12 14:57:17 +0200

    Updated solve script in dataset tests

--- datasets/tests/solve.sh
+++ datasets/tests/solve.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Solve problems using FastDownward: ./solve.sh input_dataset_number (two digits)
-fd="/home/oskopek/Dropbox/uni/year2/semester2/AP/cvi/fastdownward/fast-downward.py"
+
+#fd="/home/oskopek/Dropbox/uni/year2/semester2/AP/cvi/fastdownward/fast-downward.py"
+fd="fast-downward"
 i="$1"
 
 eval "$fd" "--build release64" "../ipc08/seq-opt/transport-strips/p$i.pddl" "--search 'astar(ipdb())'"
