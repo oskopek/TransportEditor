a1a764c89dbebe166cfb5be53db618a7c2bc98f5
Ondrej Skopek <skopekondrej@gmail.com>
2016-07-18 23:34:36 +0200

    Add Weld

---
+++ transporteditor-editor/src/main/java/com/oskopek/transporteditor/weld/EventBusProducer.java
@@ -0,0 +1,30 @@
+package com.oskopek.transporteditor.weld;
+
+import com.google.common.eventbus.EventBus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Singleton;
+
+/**
+ * A singleton Guava {@link EventBus} producer.
+ * Used primarily for constraint events.
+ */
+public final class EventBusProducer {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    /**
+     * Produce a (singleton) Guava {@link EventBus}. Used for constraint events and model changes.
+     *
+     * @return the created event bus
+     */
+    @Produces
+    @Singleton
+    public EventBus createEventBus() {
+        return new EventBus((throwable, subscriberExceptionContext) -> logger
+                .error("Exception ({}) occurred in an event subscriber ({}) for the event {}", throwable,
+                        subscriberExceptionContext.getSubscriberMethod(), subscriberExceptionContext.getEvent()));
+    }
+}
