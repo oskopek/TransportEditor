da2c4dbc4e2bf82fda527f4256551d42ecaed57a
Ondrej Skopek <skopekondrej@gmail.com>
2016-08-07 21:50:39 +0200

    Added the JUNG Graph

---
+++ transporteditor-editor/src/main/java/com/oskopek/transporteditor/planning/domain/RoadGraph.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2016 Ondrej Škopek <oskopek@oskopek.com>. All rights reserved.
+ */
+
+package com.oskopek.transporteditor.planning.domain;
+
+import edu.uci.ics.jung.graph.Graph;
+
+public interface RoadGraph extends Graph<Location, Road> {
+    // intentionally empty
+}
