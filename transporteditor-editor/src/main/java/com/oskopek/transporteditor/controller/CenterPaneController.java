265270c642070139ec3833aec976b40b41006748
Ondrej Skopek <skopekondrej@gmail.com>
2016-08-17 22:45:45 +0200

    Added proof of concept mouse click events to problem graph

--- transporteditor-editor/src/main/java/com/oskopek/transporteditor/controller/CenterPaneController.java
+++ transporteditor-editor/src/main/java/com/oskopek/transporteditor/controller/CenterPaneController.java
@@ -13,11 +13,16 @@ import javafx.fxml.FXML;
 import org.graphstream.ui.j2dviewer.J2DGraphRenderer;
 import org.graphstream.ui.swingViewer.ViewPanel;
 import org.graphstream.ui.view.Viewer;
+import org.graphstream.ui.view.ViewerListener;
+import org.graphstream.ui.view.ViewerPipe;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.lang.reflect.InvocationTargetException;
 
 @Singleton
@@ -69,16 +74,48 @@ public class CenterPaneController extends AbstractController {
         if (graph != null) {
             Viewer viewer = graph.display();
             viewer.enableAutoLayout();
+            ViewerPipe mousePipe = viewer.newViewerPipe();
+            mousePipe.addViewerListener(new MouseCatcher());
             ViewPanel viewPanel = viewer.addView("graph", new J2DGraphRenderer(), false);
-            viewer.disableAutoLayout();
+            //            viewer.disableAutoLayout();
+            viewPanel.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    super.mousePressed(e);
+                    mousePipe.pump();
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+                    super.mouseReleased(e);
+                    mousePipe.pump();
+                }
+            });
             SwingUtilities.invokeLater(() -> {
                 problemGraph.setContent(viewPanel);
                 problemGraph.setDisable(false);
             });
         }
-
-
     }
 
+    private static class MouseCatcher implements ViewerListener {
+
+        private final Logger logger = LoggerFactory.getLogger(getClass());
+
+        @Override
+        public void viewClosed(String s) {
+            logger.debug("Closed view \"{}\"", s);
+        }
+
+        @Override
+        public void buttonPushed(String s) {
+            logger.debug("Pushed node \"{}\"", s);
+        }
+
+        @Override
+        public void buttonReleased(String s) {
+            logger.debug("Released node \"{}\"", s);
+        }
+    }
 
 }
