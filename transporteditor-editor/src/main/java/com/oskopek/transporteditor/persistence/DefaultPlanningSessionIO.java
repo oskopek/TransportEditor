9c2e727f416269a74e1b88ae864ba27dd252f120
Ondrej Skopek <skopekondrej@gmail.com>
2016-08-25 01:29:26 +0200

    Added XStream as a dependency

--- transporteditor-editor/src/main/java/com/oskopek/transporteditor/persistence/DefaultPlanningSessionIO.java
+++ transporteditor-editor/src/main/java/com/oskopek/transporteditor/persistence/DefaultPlanningSessionIO.java
@@ -2,18 +2,27 @@ package com.oskopek.transporteditor.persistence;
 
 import com.oskopek.transporteditor.planning.DefaultPlanningSession;
 import com.oskopek.transporteditor.planning.PlanningSession;
+import com.thoughtworks.xstream.XStream;
 
 /**
  * Created by t-onskop on 8/21/2016.
  */
 public class DefaultPlanningSessionIO implements DataReader<DefaultPlanningSession>, DataWriter<PlanningSession> {
+
+    private final XStream xStream;
+
+    public DefaultPlanningSessionIO() {
+        xStream = new XStream();
+        xStream.setMode(XStream.ID_REFERENCES);
+    }
+
     @Override
     public String serialize(PlanningSession object) throws IllegalArgumentException {
-        return XStream.serialize(object);
+        return xStream.toXML(object);
     }
 
     @Override
     public DefaultPlanningSession parse(String contents) throws IllegalArgumentException {
-        return XStream.parse(contents);
+        return (DefaultPlanningSession) xStream.fromXML(contents);
     }
 }
