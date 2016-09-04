8b179cc60ba395d7a451c3e27d6a7299a6281d21
Ondrej Skopek <skopekondrej@gmail.com>
2016-09-04 20:57:17 +0200

    Add guava and refactoring info

--- nprg/solve-fd.sh
+++ nprg/solve-fd.sh
@@ -3,8 +3,8 @@
 # Solve Transport STRIPS problems using FastDownward.
 # Configure your fast-downward executable here:
 
-#fd="/home/oskopek/Dropbox/uni/year2/semester2/AP/cvi/fastdownward/fast-downward.py --build release64"
-fd="fast-downward --build release64"
+fd="/home/oskopek/Dropbox/uni/year2/semester2/AP/cvi/fastdownward/fast-downward.py --build release64"
+#fd="fast-downward --build release64"
 
 # For validation, please compile the VAL plan validator (Google it)
 # and put the `validate` executable on your $PATH.
