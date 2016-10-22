4c5f05ece5da97a5e7ed92a42d239c8fc4d8d845
Ondrej Skopek <skopekondrej@gmail.com>
2016-10-22 14:31:28 +0200

    Revert

--- .lfs_install.sh
+++ .lfs_install.sh
@@ -4,13 +4,11 @@
 
 LFS_VERSION=1.4.3
 build_dir="$(realpath $(pwd))"
-#domain="github.com"
-domain="192.30.253.113"
 cd /tmp
 
 echo 'install git lfs'
 filename=git-lfs-linux-amd64-"$LFS_VERSION".tar.gz
-wget https://$domain/github/git-lfs/releases/download/v"$LFS_VERSION"/"$filename" --no-check-certificate
+wget https://github.com/github/git-lfs/releases/download/v"$LFS_VERSION"/"$filename"
 tar -zxvf "$filename"
 cd "./git-lfs-$LFS_VERSION"
 . ./install.sh
