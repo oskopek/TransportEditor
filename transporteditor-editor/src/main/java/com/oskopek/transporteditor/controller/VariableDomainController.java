3166b8cd4d7ce956c7e35408e759f040bdafc847
Ondrej Skopek <skopekondrej@gmail.com>
2016-11-27 11:59:42 +0100

    Enhanced ui of VariableDomainCreatorPane

--- transporteditor-editor/src/main/java/com/oskopek/transporteditor/controller/VariableDomainController.java
+++ transporteditor-editor/src/main/java/com/oskopek/transporteditor/controller/VariableDomainController.java
@@ -3,8 +3,7 @@ package com.oskopek.transporteditor.controller;
 import com.oskopek.transporteditor.model.domain.VariableDomain;
 import com.oskopek.transporteditor.view.EnterStringDialogPaneCreator;
 import javafx.fxml.FXML;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
@@ -14,6 +13,62 @@ import javafx.scene.input.KeyEvent;
 public class VariableDomainController extends AbstractController {
 
     private Dialog<ButtonType> dialog;
+
+    @FXML
+    private RadioButton sequentialRadio;
+
+    @FXML
+    private RadioButton temporalRadio;
+
+    @FXML
+    private CheckBox fuelCheck;
+
+    @FXML
+    private CheckBox numericCheck;
+
+    @FXML
+    private CheckBox capacityCheck;
+
+    @FXML
+    private TextArea goalArea;
+
+    @FXML
+    private TextArea metricArea;
+
+    @FXML
+    private Label goalLabel;
+
+    @FXML
+    private Label metricLabel;
+
+    @FXML
+    private Label fuelLabel;
+
+    @FXML
+    private Label capacityLabel;
+
+    @FXML
+    private Label numericLabel;
+
+    private ToggleGroup group = new ToggleGroup();
+
+    @FXML
+    private void initialize() {
+        sequentialRadio.setToggleGroup(group);
+        temporalRadio.setToggleGroup(group);
+
+        numericLabel.disableProperty().bind(group.selectedToggleProperty().isNull());
+        numericCheck.disableProperty().bind(group.selectedToggleProperty().isNull());
+        capacityLabel.disableProperty().bind(group.selectedToggleProperty().isNull());
+        capacityCheck.disableProperty().bind(group.selectedToggleProperty().isNull());
+        fuelLabel.disableProperty().bind(group.selectedToggleProperty().isNull());
+        fuelCheck.disableProperty().bind(group.selectedToggleProperty().isNull());
+
+        metricArea.disableProperty().bind(numericCheck.selectedProperty().not());
+        metricLabel.disableProperty().bind(numericCheck.selectedProperty().not());
+        goalArea.disableProperty().bind(numericCheck.selectedProperty().not());
+        goalLabel.disableProperty().bind(numericCheck.selectedProperty().not());
+    }
 
     /**
      * Handles submitting the dialog in case the presses enter into the found course table.
