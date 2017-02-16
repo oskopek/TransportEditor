d7e1b3a21c6fccaf2a94e0e569eed6322a68e79c
Ondrej Skopek <skopekondrej@gmail.com>
2017-02-16 15:34:32 +0100

    Added docs to intervaltree

--- transporteditor-editor/src/main/java/com/github/kevinjdolan/intervaltree/IntervalTree.java
+++ transporteditor-editor/src/main/java/com/github/kevinjdolan/intervaltree/IntervalTree.java
@@ -12,9 +12,9 @@ import java.util.stream.Collectors;
 /**
  * An Interval Tree is essentially a map from intervals to objects, which
  * can be queried for all data associated with a particular interval of
- * time
+ * time.
  *
- * @param <Type> the type of objects to associate
+ * @param <Type> the type of data contained in the individual intervals
  * @author Kevin Dolan
  */
 public class IntervalTree<Type> {
@@ -37,7 +37,7 @@ public class IntervalTree<Type> {
      * @param intervalList the list of intervals to use
      */
     protected IntervalTree(List<Interval<Type>> intervalList) {
-        this.head = new IntervalNode<>(intervalList);
+        this.head = IntervalNode.of(intervalList);
         this.intervalList = new ArrayList<>(intervalList);
         this.inSync = true;
         this.size = intervalList.size();
@@ -45,6 +45,9 @@ public class IntervalTree<Type> {
 
     /**
      * Instantiate a new interval tree with no intervals.
+     *
+     * @param <Type_> the type of data contained in the individual intervals
+     * @return an empty interval tree
      */
     public static <Type_> IntervalTree<Type_> empty() {
         return new IntervalTree<>();
@@ -54,6 +57,8 @@ public class IntervalTree<Type> {
      * Instantiate and build an interval tree with a preset list of intervals.
      *
      * @param intervalList the list of intervals to use
+     * @param <Type_> the type of data contained in the individual intervals
+     * @return a filled in interval tree
      */
     public static <Type_> IntervalTree<Type_> of(List<Interval<Type_>> intervalList) {
         return new IntervalTree<>(intervalList);
@@ -64,16 +69,20 @@ public class IntervalTree<Type> {
      * Will rebuild the tree if out of sync.
      *
      * @param time the time to stab
+     * @return a set of data contained in the intervals that contain the given time
+     * @see Interval#contains(long)
      */
     public Set<Type> get(long time) {
         return getIntervals(time).stream().map(Interval::getData).collect(Collectors.toSet());
     }
 
     /**
-     * Perform a stabbing query, returning the interval objects
-     * Will rebuild the tree if out of sync
+     * Perform a stabbing query, returning the interval objects.
+     * Will rebuild the tree if out of sync.
      *
      * @param time the time to stab
+     * @return a set of intervals contained that contain the given time
+     * @see Interval#contains(long)
      */
     public Set<Interval<Type>> getIntervals(long time) {
         build();
@@ -81,22 +90,26 @@ public class IntervalTree<Type> {
     }
 
     /**
-     * Perform an interval query, returning the associated data
-     * Will rebuild the tree if out of sync
+     * Perform an interval query, returning the associated data.
+     * Will rebuild the tree if out of sync.
      *
      * @param start the start of the interval to check
      * @param end the end of the interval to check
+     * @return a set of data contained in the intervals that intersect the given [start, end] interval
+     * @see Interval#intersects(Interval)
      */
     public Set<Type> get(long start, long end) {
         return getIntervals(start, end).stream().map(Interval::getData).collect(Collectors.toSet());
     }
 
     /**
-     * Perform an interval query, returning the interval objects
-     * Will rebuild the tree if out of sync
+     * Perform an interval query, returning the interval objects.
+     * Will rebuild the tree if out of sync.
      *
      * @param start the start of the interval to check
      * @param end the end of the interval to check
+     * @return a set of intervals that intersect the given [start, end] interval
+     * @see Interval#intersects(Interval)
      */
     public Set<Interval<Type>> getIntervals(long start, long end) {
         build();
@@ -104,8 +117,8 @@ public class IntervalTree<Type> {
     }
 
     /**
-     * Add an interval object to the interval tree's list
-     * Will not rebuild the tree until the next query or call to build
+     * Add an interval object to the interval tree's list.
+     * Will not rebuild the tree until the next query or call to build.
      *
      * @param interval the interval object to add
      */
@@ -115,8 +128,8 @@ public class IntervalTree<Type> {
     }
 
     /**
-     * Add an interval object to the interval tree's list
-     * Will not rebuild the tree until the next query or call to build
+     * Add an interval object to the interval tree's list.
+     * Will not rebuild the tree until the next query or call to build.
      *
      * @param begin the beginning of the interval
      * @param end the end of the interval
@@ -128,53 +141,44 @@ public class IntervalTree<Type> {
     }
 
     /**
-     * Determine whether this interval tree is currently a reflection of all intervals in the interval list
+     * Determine whether this interval tree is currently a reflection of all intervals in the interval list.
      *
-     * @return true if no changes have been made since the last build
+     * @return true iff no changes have been made since the last build
      */
     public boolean inSync() {
         return inSync;
     }
 
     /**
-     * Build the interval tree to reflect the list of intervals,
-     * Will not run if this is currently in sync
+     * Build the interval tree to reflect the list of intervals.
+     * Will not run if this is currently in sync.
      */
     public void build() {
         if (!inSync) {
-            head = new IntervalNode<>(intervalList);
+            head = IntervalNode.of(intervalList);
             inSync = true;
             size = intervalList.size();
         }
     }
 
     /**
+     * The number of intervals represented by this tree.
+     *
      * @return the number of entries in the currently built interval tree
+     * @see #listSize()
      */
     public int currentSize() {
         return size;
     }
 
     /**
+     * The number of intervals in the internal list.
+     *
      * @return the number of entries in the interval list, equal to .size() if inSync()
+     * @see #currentSize()
      */
     public int listSize() {
         return intervalList.size();
-    }
-
-    private String nodeString(IntervalNode<Type> node, int level) {
-        if (node == null) {
-            return "";
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < level; i++) {
-            sb.append("\t");
-        }
-        sb.append(node).append("\n");
-        sb.append(nodeString(node.getLeft(), level + 1));
-        sb.append(nodeString(node.getRight(), level + 1));
-        return sb.toString();
     }
 
     @Override
@@ -196,6 +200,6 @@ public class IntervalTree<Type> {
 
     @Override
     public String toString() {
-        return nodeString(head, 0);
+        return "IntervalTree[" + (head == null ? "" : head.toText(0)) + "]";
     }
 }
---
+++ transporteditor-editor/src/main/java/com/github/kevinjdolan/intervaltree/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * A simple interval tree implementation, derived from: https://github.com/kevinjdolan/intervaltree.
+ */
+package com.github.kevinjdolan.intervaltree;
