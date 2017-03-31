5eca69480babc0ae8e72f2ee34b63845c41b08f0
Ondrej Skopek <skopekondrej@gmail.com>
2017-03-31 13:03:30 +0200

    Fixed release script + gitlab-ci config

---
+++ calculate_coverage.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+if [ -z "$1" ]; then
+reportFolder="jacoco"
+else
+reportFolder="jacoco-it"
+fi
+
+for file in `find . -wholename '.*'"$reportFolder"'/jacoco.xml'`; do
+cat $file | grep -E '<counter type="INSTRUCTION"[^>]*/>' -o | tail -n 1 | sed -E 's@<counter type="INSTRUCTION"[ ]+missed="([0-9]+)"[ ]+covered="([0-9]+)"[ ]*/>@\2 (\1+\2)@'
+done |
+awk '
+{
+    for (i=1; i<=NF; i++)  {
+        a[NR,i] = $i
+    }
+}
+NF>p { p = NF }
+END {
+    for(j=1; j<=p; j++) {
+        str=a[1,j]
+        for(i=2; i<=NR; i++){
+            str=str" "a[i,j];
+        }
+        print str
+    }
+}' | tr ' \n' '+/' | sed 's@/$@@;s@^@(@;s@$@)@;s@/@)/(@;s@^@scale=2;@;s@$@\n@' | bc
