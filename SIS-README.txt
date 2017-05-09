fc480a4c9a2aa5f33295831c24dc20b6f480a221
Ondrej Skopek <skopekondrej@gmail.com>
2017-05-09 15:40:41 +0200

    Added sis-convert script

---
+++ SIS-README.txt
@@ -0,0 +1,8 @@
+IMPORTANT
+=========
+
+In case a "sis-unconvert.sh.txt" or "sis-unconvert.bat.txt" is present in this
+directory, you have obtained a SIS-compatible version of this work.
+To convert it back into a regular version, run either "sis-unconvert.sh.txt"
+or "sis-unconvert.bat.txt", depending on your platform.
+
---
+++ tools/release/sis-convert.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Author: Ondrej Skopek <oskopek@oskopek.com>
+# License: MIT <https://opensource.org/licenses/MIT>
+#
+# Run this script using no parameters, or a single parameter (the root directory you wish to convert)
+# NOTE: This script might irreversibly damage your files. Please, back up your files.
+# NOTE 2: If you find any bugs, please report/fix them at:
+#                 https://gist.github.com/oskopek/cca1a553c612e24f8326e6bc0b5d84f7
+
+
+set -e
+
+root_dir="$1"
+if [ -z "$root_dir" ]; then
+root_dir="." # root directory to recursively convert
+fi
+whitelist="pdf jpg wav wave mp3 vob mp4 csv xml txt" # whitelist of all file endings
+uninstall_file="sis-unconvert.sh.txt"
+uninstall_file_win="sis-unconvert.bat.txt"
+
+files="`find "$root_dir" -not \( -name '\.git' -type d -prune \) -type f`"
+for ending in $whitelist; do
+    files="`echo "$files" | grep -v "\.$ending\$"`"
+done
+
+echo '#!/bin/bash' > "$uninstall_file"
+chmod +x "$uninstall_file"
+echo '@echo off' > "$uninstall_file_win"
+chmod +x "$uninstall_file_win"
+for file in $files; do
+    mv -v "$file" "$file".txt
+    echo mv -v "$file".txt "$file" >> "$uninstall_file"
+    echo 'move /y' "$file".txt "$file" >> "$uninstall_file_win"
+done
